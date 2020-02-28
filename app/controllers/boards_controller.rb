9284b97b439efb6b816c69b05ad97627a2251805
araihara-k <araihara@gmail.com>
2020-02-28 19:38:06 +0900

    refactoring board_controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,6 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
+
   def index
     @boards = Board.all
   end
@@ -8,27 +10,23 @@ class BoardsController < ApplicationController
   end
   
   def create
-    board = Board.create(board_params)
-    redirect_to board
+    @board = Board.create(board_params)
+    redirect_to @board
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    board = Board.find(params[:id])
-    board.update(board_params)
-    redirect_to board
+    @board.update(board_params)
+    redirect_to @board
   end
 
   def destroy
-    board = Board.find(params[:id])
-    board.delete
+    @board.delete
     redirect_to boards_path
   end
 
@@ -36,5 +34,9 @@ class BoardsController < ApplicationController
   
   def board_params
     params.require(:board).permit(:name, :title, :comment)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
   end
 end
