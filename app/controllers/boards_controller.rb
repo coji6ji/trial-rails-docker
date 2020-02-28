cebe29b149c48a5e78e4cc732e87b4e326268463
araihara-k <araihara@gmail.com>
2020-02-28 19:10:49 +0900

    implement boards/edit

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,12 +10,23 @@ class BoardsController < ApplicationController
   def create
     Board.create(board_params)
   end
-  
+
   def show
     @board = Board.find(params[:id])
   end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    board = Board.find(params[:id])
+    board.update(board_params)
+
+    redirect_to board
+  end
   
-  private 
+  private
   
   def board_params
     params.require(:board).permit(:name, :title, :comment)
