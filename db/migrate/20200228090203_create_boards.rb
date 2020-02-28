fefb6a6b56cc913c8d71f7d1ac69bb5900f9bec0
araihara-k <araihara@gmail.com>
2020-02-28 18:05:02 +0900

    implement boards/new

--- db/migrate/20200228090203_create_boards.rb
+++ db/migrate/20200228090203_create_boards.rb
@@ -1,8 +1,10 @@
 class CreateBoards < ActiveRecord::Migration[5.0]
   def change
+    drop_table :boards
     create_table :boards do |t|
       t.string :name
       t.string :title
+      t.text :comment
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200228082005) do
+ActiveRecord::Schema.define(version: 20200228090203) do
 
   create_table "boards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "comment",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
 end
